f9a4a67aea00419bcc6a2e4239a0c1e5c322060f
Boris Evdeev <evdeev@gmail.com>
2026-05-20 19:56:19 +0300

    Add Statistics page component

---
+++ src/pages/StatisticsPage.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+export function StatisticsPage() {
+  return (
+    <div
+      style={{
+        padding: '32px 20px',
+        color: '#8e8e93',
+        fontSize: '17px'
+      }}
+    >
+      Статистика появится позже
+    </div>
+  )
+}
