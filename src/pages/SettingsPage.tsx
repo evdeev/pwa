47e685af9a171e1719cf0f2a1786ec654984f7b6
Boris Evdeev <evdeev@gmail.com>
2026-05-20 19:56:33 +0300

    Add Settings page component

---
+++ src/pages/SettingsPage.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+export function SettingsPage() {
+  return (
+    <div
+      style={{
+        padding: '32px 20px',
+        color: '#8e8e93',
+        fontSize: '17px'
+      }}
+    >
+      Настройки появятся позже
+    </div>
+  )
+}
