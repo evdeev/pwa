52c2bb8649658226d683997f144aa40f323c6638
Boris Evdeev <evdeev@gmail.com>
2026-05-21 00:00:34 +0300

    Connect legacy HistoryPage to feature-first history module

--- src/pages/HistoryPage.tsx
+++ src/pages/HistoryPage.tsx
@@ -1,11 +1,1 @@
-import { testSessions } from '../entities/session/testData'
-import { SessionList } from '../features/sessions/ui/SessionList'
-
-export function HistoryPage() {
-  return (
-    <section className="page-section" aria-label="История тренировок">
-      <div className="month-header">Май 2026</div>
-      <SessionList sessions={testSessions} />
-    </section>
-  )
-}
+export { HistoryPage } from '../features/history/ui/HistoryPage'
