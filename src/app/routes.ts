a3eadab59b9c5273be5aba6ef1dfeb310e46c863
Boris Evdeev <evdeev@gmail.com>
2026-05-20 19:55:49 +0300

    Add Framework7 routes configuration

---
+++ src/app/routes.ts
@@ -0,0 +1,14 @@
+export const routes = [
+  {
+    path: '/',
+    component: 'history'
+  },
+  {
+    path: '/statistics/',
+    component: 'statistics'
+  },
+  {
+    path: '/settings/',
+    component: 'settings'
+  }
+]
