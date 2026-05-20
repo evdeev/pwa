8f25c4d4cbc1f08562bc23076492f90d00d39d47
Boris Evdeev <evdeev@gmail.com>
2026-05-21 00:05:17 +0300

    Use shared PageLayout inside history feature

--- src/features/history/ui/HistoryPage.tsx
+++ src/features/history/ui/HistoryPage.tsx
@@ -1,16 +1,12 @@
-import styles from './HistoryPage.module.scss'
+import { PageLayout } from '../../../shared/layouts/PageLayout/PageLayout'
 
 import { testSessions } from '../../../entities/session/testData'
 import { SessionList } from '../../sessions/ui/SessionList'
 
 export function HistoryPage() {
   return (
-    <div className={styles.page}>
-      <section className={styles.section}>
-        <div className={styles.monthHeader}>МАЙ 2026</div>
-
-        <SessionList sessions={testSessions} />
-      </section>
-    </div>
+    <PageLayout sectionTitle="МАЙ 2026">
+      <SessionList sessions={testSessions} />
+    </PageLayout>
   )
 }
