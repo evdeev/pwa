6d5c5086722dec7d591d1740e68bec85ac774b7f
Boris Evdeev <evdeev@gmail.com>
2026-05-22 00:56:49 +0300

    feat(history): add floating action button

--- src/features/history/ui/HistoryPage.tsx
+++ src/features/history/ui/HistoryPage.tsx
@@ -1,4 +1,6 @@
 import { PageLayout } from '../../../shared/layouts/PageLayout/PageLayout'
+import { FloatingActionButton } from '../../../shared/ui/FloatingActionButton/FloatingActionButton'
+import { PlusIcon } from '../../../shared/ui/FloatingActionButton/PlusIcon'
 import { PageSection } from '../../../shared/ui/PageSection/PageSection'
 
 import {
@@ -11,18 +13,22 @@ import { SessionGroup } from '../../sessions/ui/SessionGroup'
 
 export function HistoryPage() {
   return (
-    <PageLayout>
-      <PageSection title="МАЙ 2026" compact>
-        <SessionGroup sessions={maySessions} />
-      </PageSection>
+    <>
+      <PageLayout>
+        <PageSection title="МАЙ 2026" compact>
+          <SessionGroup sessions={maySessions} />
+        </PageSection>
 
-      <PageSection title="АПРЕЛЬ 2026">
-        <SessionGroup sessions={aprilSessions} />
-      </PageSection>
+        <PageSection title="АПРЕЛЬ 2026">
+          <SessionGroup sessions={aprilSessions} />
+        </PageSection>
 
-      <PageSection title="МАРТ 2026">
-        <SessionGroup sessions={marchSessions} />
-      </PageSection>
-    </PageLayout>
+        <PageSection title="МАРТ 2026">
+          <SessionGroup sessions={marchSessions} />
+        </PageSection>
+      </PageLayout>
+
+      <FloatingActionButton aria-label="Добавить тренировку" icon={<PlusIcon />} />
+    </>
   )
 }
