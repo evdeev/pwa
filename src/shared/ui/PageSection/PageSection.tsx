6b926de49c4d05be8d60131d4d849ad4388cf03f
Boris Evdeev <evdeev@gmail.com>
2026-05-22 00:24:06 +0300

    refactor(ui): reuse section header primitive

--- src/shared/ui/PageSection/PageSection.tsx
+++ src/shared/ui/PageSection/PageSection.tsx
@@ -1,5 +1,7 @@
 import type { PropsWithChildren } from 'react'
 
+import { SectionHeader } from '../SectionHeader/SectionHeader'
+
 import styles from './PageSection.module.scss'
 
 interface PageSectionProps extends PropsWithChildren {
@@ -10,7 +12,7 @@ interface PageSectionProps extends PropsWithChildren {
 export function PageSection({ title, compact = false, children }: PageSectionProps) {
   return (
     <section className={compact ? styles.compactSection : styles.section}>
-      <div className={styles.title}>{title}</div>
+      <SectionHeader>{title}</SectionHeader>
 
       <div className={styles.content}>{children}</div>
     </section>
